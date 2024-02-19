23df9fa351b675f026f3c974c483dec651592c3b
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-02-19 11:54:35 +0000

    Delete faulty space

--- drf_api/drf_api/settings.py
+++ drf_api/drf_api/settings.py
@@ -131,14 +131,14 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 #     }
 # }
 
- if 'DEV' in os.environ:
+if 'DEV' in os.environ:
      DATABASES = {
          'default': {
              'ENGINE': 'django.db.backends.sqlite3',
              'NAME': BASE_DIR / 'db.sqlite3',
          }
      }
- else:
+else:
      DATABASES = {
          'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
      }
