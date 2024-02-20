961ac0ea40ea2b84ccc3606c29e60dd0a37b069b
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-02-20 10:13:01 +0000

    Troubleshoot to be to migrate

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -54,7 +54,7 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6wb#ip0z2!i%o*pqxqwu4wmwrn)l_y!dnzo_k=#f*@%ttv2e!o'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -131,18 +131,16 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 #     }
 # }
 
-if 'DEV' in os.environ:
-     DATABASES = {
-         'default': {
-             'ENGINE': 'django.db.backends.sqlite3',
-             'NAME': BASE_DIR / 'db.sqlite3',
-         }
-     }
-else:
-     DATABASES = {
-         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-     }
-     print('connected')
+DATABASES = {
+    'default': ({
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    } if 'DEV' in os.environ 
+    else dj_database_url.parse(
+        os.environ.get('DATABASE_URL')
+    )
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
