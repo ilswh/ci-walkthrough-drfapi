2a5643d7dbcd5919507a66d9b8d3ab46d39c3995
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-02-23 12:02:59 +0000

    Try to fix bugs

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import allauth
 
 if os.path.exists('env.py'):
     import env
@@ -82,6 +83,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
+    
 
     'profiles',
     'posts',
@@ -132,29 +134,18 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': ({
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    } if 'DEV' in os.environ 
-    else dj_database_url.parse(
-        os.environ.get('DATABASE_URL')
-    )
-    )
-}
-
-#  if 'DEV' in os.environ:
-#      DATABASES = {
-#          'default': {
-#              'ENGINE': 'django.db.backends.sqlite3',
-#              'NAME': BASE_DIR / 'db.sqlite3',
-#          }
-#      }
-#  else:
-#      DATABASES = {
-#          'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-#      }
-    # print('connected')
+if 'DEV' in os.environ:
+     DATABASES = {
+         'default': {
+             'ENGINE': 'django.db.backends.sqlite3',
+             'NAME': BASE_DIR / 'db.sqlite3',
+         }
+     }
+else:
+     DATABASES = {
+         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+     }
+print('connected')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
